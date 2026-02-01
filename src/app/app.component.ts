5e5684f288a130ae06ce52a501a9b742e9101715
frederico nakajima <fredericonakajimanakajima@gmail.com>
2026-02-01 18:25:02 -0300

    criação do layout do modal de criação e edição de tarefa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { WelcomeSectionComponent } from "./components/welcome-section/welcome-section.component";
 import { TaskListSectionComponent } from "./components/task-list-section/task-list-section.component";
+import { TaskFormModalComponent } from "./components/task-form-modal/task-form-modal.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, WelcomeSectionComponent, TaskListSectionComponent],
+  imports: [HeaderComponent, WelcomeSectionComponent, TaskListSectionComponent, TaskFormModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
