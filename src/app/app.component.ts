173adba35f53261fa3588d9552587b64f6980af5
frederico nakajima <fredericonakajimanakajima@gmail.com>
2026-02-01 09:54:09 -0300

    criação do layout das colunas de tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { WelcomeSectionComponent } from "./components/welcome-section/welcome-section.component";
+import { TaskListSectionComponent } from "./components/task-list-section/task-list-section.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, WelcomeSectionComponent],
+  imports: [HeaderComponent, WelcomeSectionComponent, TaskListSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
