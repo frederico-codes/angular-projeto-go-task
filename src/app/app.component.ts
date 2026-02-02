a423a95617e23c3b2dbed1cc3908cec49b1da3a8
frederico nakajima <fredericonakajimanakajima@gmail.com>
2026-02-02 07:39:49 -0300

    criação do layout do modal de comentários da tarefa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { HeaderComponent } from "./components/header/header.component";
 import { WelcomeSectionComponent } from "./components/welcome-section/welcome-section.component";
 import { TaskListSectionComponent } from "./components/task-list-section/task-list-section.component";
 import { TaskFormModalComponent } from "./components/task-form-modal/task-form-modal.component";
+import { TaskCommentsModalComponent } from "./components/task-comments-modal/task-comments-modal.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, WelcomeSectionComponent, TaskListSectionComponent, TaskFormModalComponent],
+  imports: [
+    HeaderComponent,
+    WelcomeSectionComponent,
+    TaskListSectionComponent,
+    TaskFormModalComponent,
+    TaskCommentsModalComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
